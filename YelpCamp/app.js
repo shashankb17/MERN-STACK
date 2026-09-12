bb3599805027799e93f66a46bb0d3a38b68c9537
shashankb17 <b.shashank9979@gmail.com>
2026-09-13 02:13:04 +0530

    Full Crud Operations on Yelp Camp

---
+++ YelpCamp/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+const methodOverride = require("method-override");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/yelp-camp")
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database Connection Error: ");
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const newCamp = new Campground(req.body.campground);
+  await newCamp.save();
+  res.redirect(`/campgrounds/${newCamp._id}`);
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    { ...req.body.campground },
+    {
+      runValidators: true,
+      new: true,
+    },
+  );
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.listen(3000, () => {
+  console.log("Serving on Port 3000");
+});
